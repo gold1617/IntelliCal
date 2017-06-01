c7366a5bb96c7cd395910ce0ba40b05b76994960
Rishab Chhabra <rishab.chhabra@stonybrook.edu>
2017-06-01 15:51:33 -0500

    Added storing credentials by session

--- app.py
+++ app.py
@@ -12,30 +12,33 @@ from pytz import timezone
 
 import dateutil.parser as tp
 
-
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,session
 import json
 
 app = Flask(__name__)
 
+
 json_file = open('paths.json').read()
 paths = json.loads(json_file)
 redir = paths['redir']
-
+app.secret_key = paths['secret_key']
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = paths['client_secret']
 APPLICATION_NAME = 'IntelliCal'
 
 def get_credentials(authCode):
-    credentials = client.credentials_from_clientsecrets_and_code(CLIENT_SECRET_FILE,SCOPES,authCode,redirect_uri=redir)
     
-    return credentials
+    if(session.get("credentials",None) == None or session.get('auth',None) != authCode):
+        session['credentials'] = client.credentials_from_clientsecrets_and_code(CLIENT_SECRET_FILE,SCOPES,authCode,redirect_uri=redir).to_json()
+        session['authCode'] = authCode
+    
+    return session['credentials']
 
 @app.route('/calendars',methods=['POST'])
 def calendars():
     server_auth = request.form['ServerAuth']
 
-    credentials = get_credentials(server_auth)
+    credentials = client.OAuth2Credentials.from_json(get_credentials(server_auth))
     
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
@@ -53,7 +56,13 @@ def calendars():
 
 @app.route('/test')
 def test():
-    return 'Test Successful'
+    session['test'] = 'Test Successful'
+    return session.get('test','failure')
+
+@app.route('/test2')
+def test2():
+    res = session.get('test','Session not found')    
+    return res
 
 
 if __name__ == "__main__":
